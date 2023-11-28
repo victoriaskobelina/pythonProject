7e6c19a7751fb3b81e280f5b30d4d9f7d3fe28fa
victoriaskobelina <v.skobelina1@bk.ru>
2023-11-28 10:54:16 +0300

    PZ_4

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,1 +1,15 @@
 #Дано целое число N(>0). Найти сумму N^2+(N+1)^2+(N+2)^2+...+(2N)^2
+def calculate(N):
+    sum = 0
+    i = N
+    while i <= 2*N:
+        sum += i**2
+        i += 1
+    return sum
+
+try:
+    N = int(input("Введите целое число N: "))
+    result = calculate(N)
+    print("Сумма равна", result)
+except ValueError:
+    print("Введите целое число, пожалуйста")
