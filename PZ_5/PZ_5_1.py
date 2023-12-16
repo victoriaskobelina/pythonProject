b6204f0870efee35df914cd64f514757803efe8b
victoriaskobelina <v.skobelina1@bk.ru>
2023-12-16 11:01:46 +0300

    PZ_5

---
+++ PZ_5/PZ_5_1.py
@@ -0,0 +1,22 @@
+#Составить функцию решения задачи: из заданного числа вычли сумму его цифр.
+#Из результата вновь вычли сумму его цифр и т. д. Через сколько таких дейтсвий получится нуль?
+def sum_of_digits(number):
+    sum = 0
+    while number != 0:
+        sum += number % 10  # получаем последнюю цифру числа и прибавляем к сумме
+        number //= 10  # удаляем последнюю цифру числа
+    return sum
+
+def steps_to_zero(number):
+    steps = 0
+    while number != 0:
+        number -= sum_of_digits(number)
+        steps += 1
+    return steps
+
+try:
+    number = int(input("Введите число: "))
+    result = steps_to_zero(number)
+    print("Количество действий для достижения нуля: ", result)
+except ValueError:
+    print("Введите, пожалуйста, целое число")
