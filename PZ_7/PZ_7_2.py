b46be64bfcb03237a0a84aabc5e4cac4df2b2f45
victoriaskobelina <v.skobelina1@bk.ru>
2023-12-22 06:59:30 +0300

    PZ_6 and PZ_7

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -1,15 +1,15 @@
 #Дана строка, содержащая по крайней мере один символ пробела.
 #Вывести подстроку, расположенную между первым и последним пробелом исходной строки.
 #Если строка содержит только один пробел, то вывести пустую строку.
-def get_substring_between_spaces(input_string):
+def string_between_spaces(input_string):
     # Находим индексы первого и последнего пробела в строке
-    first_space_index = input_string.find(' ')
-    last_space_index = input_string.rfind(' ')
+    first_index = input_string.find(' ')
+    last_index = input_string.rfind(' ')
     # Если строка содержит только один пробел, возвращаем пустую строку
-    if first_space_index == last_space_index:
+    if first_index == last_index:
         return ""
     # Иначе возвращаем подстроку, находящуюся между первым и последним пробелом
-    return input_string[first_space_index + 1:last_space_index]
+    return input_string[first_index + 1:last_index]
 input_string = "Mother drinks meat"
-result = get_substring_between_spaces(input_string)
+result = string_between_spaces(input_string)
 print(result)
