21b06a2db49e47919b8a7d70cdd5432e67637fd1
victoriaskobelina <v.skobelina1@bk.ru>
2024-01-23 16:25:22 +0300

    PZ_9

---
+++ PZ_9/PZ_9.py
@@ -0,0 +1,20 @@
+#Организовать словарь 10 русско-английских слов,
+#обеспечивающий перевод русского слова на английский
+dictionary = {
+    "яблоко": "apple",
+    "банан": "banana",
+    "вишня": "cherry",
+    "апельсин": "orange",
+    "мандарин": "grape",
+    "киви": "kiwi",
+    "черника": "blueberry",
+    "ежевика": "blackberry",
+    "лимон": "lemon",
+    "лайм": "lime"
+}
+russian_word = input("Введите, пожалуйста, русское слово: ")
+if russian_word in dictionary:
+    translation = dictionary[russian_word]
+    print("Перевод слова", russian_word, "на английский:", translation)
+else:
+    print("Данного слова нет в словаре")
