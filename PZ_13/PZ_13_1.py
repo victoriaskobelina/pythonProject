51ac4f71677d2db14df9c96f3e023cd80445e6d5
victoriaskobelina <v.skobelina1@bk.ru>
2024-02-28 10:50:44 +0300

    PZ_13

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -1,11 +1,10 @@
+#В матрице элементы последней строки заменить на 0.
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-
 for i in range(len(matrix[-1])):
     matrix[-1][i] = 0
-
 for row in matrix:
     print(row)
