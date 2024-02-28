15b37f637f6d193947d2d275e7c04212bff78e46
victoriaskobelina <v.skobelina1@bk.ru>
2024-02-28 14:10:19 +0300

    PZ_12

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -3,11 +3,9 @@
 import random
 numbers = random.choices(range(1, 20), k=20)
 print("Список до обработки:", numbers)
-#множество для уникальных элементов
 unique = set(numbers)
-#находим количество уникальных элементов
 unique_numbers = len(unique)
-#увеличиваем элементы больше 5 на два раза
+#увеличиваем элементы больше 5 в два раза
 for i in range(len(numbers)):
     if numbers[i] > 5:
         numbers[i] *= 2
