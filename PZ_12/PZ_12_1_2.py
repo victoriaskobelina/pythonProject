636bc89d948c65bea90e201df2d05c09b6fa73d8
victoriaskobelina <v.skobelina1@bk.ru>
2024-03-13 14:09:44 +0300

    PZ

---
+++ PZ_12/PZ_12_1_2.py
@@ -0,0 +1,11 @@
+#Организовать и вывести последовательность из 20 целых чисел, выбрать не
+#повторяющиеся элементы, найти их количество. Элементы больше 5 увеличить в два раза.
+import random
+sequence = [random.randint(1, 10) for _ in range(20)]
+print("Исходная последовательность:", sequence)
+unique_elements = list(set(sequence))
+print("Неповторяющиеся элементы:", unique_elements)
+n_of_un_elements = len(unique_elements)
+print("Количество неповторяющихся элементов:", n_of_un_elements)
+up_sequence = [x * 2 if x > 5 else x for x in unique_elements]
+print("Последовательность с элементами больше 5, увеличенными в два раза:", up_sequence)
