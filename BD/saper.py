6dc97c85873789c0c66a44343b386c8d63cfd63e
victoriaskobelina <v.skobelina1@bk.ru>
2024-04-06 11:07:31 +0300

    BD

--- BD/saper.py
+++ BD/saper.py
@@ -12,15 +12,49 @@ with sq.connect('saper.db') as con:
         score INTEGER
     )""")
 
-# with sq.connect('saper.db') as con:
-#     cur = con.cursor()
-#     cur.execute("INSERT INTO users VALUES (1, 'Алексей', 1, 22, 1000)")
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
 
-cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?", info_users)
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users ")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE score < 1000 ORDER BY old DESC ")
+    result = cur.fetchall()
+    print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE old BETWEEN 19 AND 32 AND score < 1000 ")
+    result = cur.fetchall()
+    print(result)
 
 
 with sq.connect('saper.db') as con:
     cur = con.cursor()
-    cur.execute("SELECT * FROM users")
+    cur.execute("SELECT * FROM users WHERE sex = 2 ")
     result = cur.fetchall()
     print(result)
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE users SET score = score*2 WHERE sex = 2")
+    cur.execute("SELECT * FROM users WHERE sex = 2 ")
+    result = cur.fetchall()
+    print(result)
+
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute("UPDATE users SET score = score+100000 WHERE name LIKE 'М%'")
+    cur.execute("SELECT * FROM users WHERE sex = 2 ")
+    result = cur.fetchall()
+    print(result)
+
+
+
---
+++ PZ_15/PZ_15.py
@@ -0,0 +1,25 @@
+#Приложение ПАРИКМАХЕРСКАЯ для некоторой организации. БД должна
+#содержать таблицу Услуги со следующей структурой записи: ФИО мастера, ФИО клиента,
+#пол, название стрижки, стоимость.
+import sqlite3 as sq
+from salon import info_salon
+
+with sq.connect('salon.db') as con:
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS services")
+    cur.execute("""CREATE TABLE IF NOT EXISTS services(
+        fullname_master TEXT PRIMARY KEY, 
+        fullname_client TEXT NOT NULL,
+        sex TEXT NOT NULL,
+        haircut_name TEXT NOT NULL,
+        price DECIMAL(5,2) NOT NULL
+    )""")
+with sq.connect('salon.db') as con:
+    cur = con.cursor()
+    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", salon.py)
+
+with sq.connect('salon.db') as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM")
+    result = cur.fetchall()
+    print(result)
