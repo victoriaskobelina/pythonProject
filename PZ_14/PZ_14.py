f25bb7830aeb0eb363e824450bf531984e7b2735
victoriaskobelina <v.skobelina1@bk.ru>
2024-04-27 11:57:23 +0300

    PZ_14

--- PZ_14/PZ_14.py
+++ PZ_14/PZ_14.py
@@ -11,7 +11,7 @@ count_dates_dot = len(dates_dot)
 count_dates_slash = len(dates_slash)
 #Даты февраля в формате ДД/ММ/ГГГГ
 f_dates_slash = re.findall(r'\b(0[1-9]|[12][0-9]|3[01])/(0[2])/(\d{4})\b', data)
-#Найденные даты февраля в новый файл
+#Найденные даты в новый файл
 with open('dates_slash.txt', 'w') as file:
     for date in f_dates_slash:
         file.write('/'.join(date) + '\n')
