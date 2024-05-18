35d3357b649542af8c10f1d9b32e83a9cc1d7435
victoriaskobelina <v.skobelina1@bk.ru>
2024-05-18 20:36:58 +0300

    Lalala

--- Practic/Practic.py
+++ Practic/Practic.py
@@ -23,13 +23,12 @@ if р.search(d):
     print("Дата введена правильно")
 else:
     print("Дата введена неправильно")
-# Выведет: Дата введена неправильно
+# Выведет: дата введена неправильно
 
 import re
 p = re.compile(r"^.+$") # Точка не соответствует \n
 print(p.findall("str1\nstr2\nstr3")) # Ничего не найдено []
 p = re.compile(r"^.+$", re.S) # Теперь точка соответствует \n
-print(p.findall("str1\nstr2\nstr3")) #Строка полностью соответствует['str1\nstr2\nstr3']
+print(p.findall("str1\nstr2\nstr3")) #Строкаполностьюсоответствует['str1\nstr2\nstr3']
 p = re.compile(r"^.+$", re.M) # Многострочный режим
 print(p.findall("str1\nstr2\nstr3")) # Получили каждую подстроку ['str1', 'str2', 'str3']
-
