5e5094417e9c8c8264723873de3d84d9fa60a862
victoriaskobelina <v.skobelina1@bk.ru>
2024-06-18 17:42:59 +0300

    OOP

--- Practic/OOP_exercise_3.py
+++ Practic/OOP_exercise_3.py
@@ -3,3 +3,15 @@
 #Увеличение количества созданных экземпляров обеспечить в конструкторе __init__
 #Статический метод total_people должен обеспечивать построение и вывод фразы
 #о количестве созданных экземпляров
+class Person:
+    count = 0
+    def __init__(self):
+        Person.count += 1
+    @staticmethod
+    def total_people():
+        print(f"Общее количество: {Person.count}")
+person1 = Person()
+person2 = Person()
+person3 = Person()
+
+Person.total_people()
